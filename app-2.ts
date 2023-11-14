3749e95309ed2ae05f0efe375b5aaa2c9d5c4c82
Victor <VictorCarmona9035@hotmail.com>
2023-11-14 15:59:46 +0100

    4-Organizando el proyecto

---
+++ app-2.ts
@@ -0,0 +1,41 @@
+import { Action } from './ngrx-fake/ngrx';
+import { incrementadorAction, decrementadorAction, multiplicadorAction, dividirAction, resetAction } from './contador/contador.actions';
+
+function reducer(state = 10, action: Action) {
+
+    switch (action.type) {
+        case 'INCREMENTAR':
+            return state += 1;
+
+        case 'DECREMENTAR':
+            return state -= 1;
+
+        case 'MULTIPLICAR':
+            return state * action.payload;
+
+        case 'DIVIDIR':
+            return state / action.payload;
+
+        case 'RESET':
+            return state = 0 ;
+
+        default:
+            return state;
+    }
+}
+
+//Usra el reducer
+console.log(reducer(10, incrementadorAction));
+
+
+
+console.log(reducer(10, decrementadorAction));
+
+
+
+console.log(reducer(10, multiplicadorAction));
+
+
+console.log(reducer(10, dividirAction));
+
+console.log(reducer(10, resetAction));
