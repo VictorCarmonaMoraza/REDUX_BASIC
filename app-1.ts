25669a8ef890a85faaaf5c9b204a267d4c49fe7f
Victor <VictorCarmona9035@hotmail.com>
2023-11-14 12:25:34 +0100

    2-Acciones y Reducer basico

--- app-1.ts
+++ app-1.ts
@@ -1,1 +1,28 @@
-console.log('Hola Mundo');
+
+//Acciones
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+// const incrementadorAction: Action = {
+//     type: 'INCREMENTAR'
+// };
+
+
+//DEvuelve el nuevo estado
+function reducer(state = 10, action: Action) {
+
+    if(action.type ==='INCREMENTAR' ){
+        return state + 1;
+    }
+    return state;
+
+}
+
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+
+//Usra el reducer
+console.log(reducer(10,incrementadorAction));
